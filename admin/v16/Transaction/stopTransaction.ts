220095eb11d2dadc0545a1145be5a3d8c54531b3
Stuart Lowe <stuart.lowe@zenobe.com>
2025-07-25 10:49:19 +0100

    feat: transaction id arg in stop transaction

--- admin/v16/Transaction/stopTransaction.ts
+++ admin/v16/Transaction/stopTransaction.ts
@@ -1,11 +1,19 @@
 import * as uuid from "uuid";
 import { sendAdminCommand } from "../../admin";
 
+let transactionId = 1;
+
+if (process.argv[2]) {
+	transactionId = Number(process.argv[2]);
+}
+
 sendAdminCommand({
   action: "StopTransaction",
   messageId: uuid.v4(),
   payload: {
-    transactionId: 1,
+    idTag: "",
+    // reason: "Other",
+    transactionId: transactionId,
     timestamp: new Date(),
     meterStop: 2000,
   },
